4910d939d170cb1e99d7db0c5c47537d2c3833cc
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-07-15 13:59:14 +0530

    Update BlogTableItem.jsx

--- components/AdminComponents/BlogTableItem.jsx
+++ components/AdminComponents/BlogTableItem.jsx
@@ -8,7 +8,7 @@ const BlogTableItem = ({authorImg,title,author,date,deleteBlogs,mongoId} ) => {
   return (
     <tr className='bg-white  border-b  '>
         <th scope='row' className='items-center gap-3  hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
-         <Image width={40} height={40} src={authorImg?authorImg:assets.profile_icon}/>
+         <Image width={40} height={40} src={authorImg?authorImg:assets.profile_icon} alt=''/>
          <p>{author?author:"No author"}</p>
         </th>
         <td className='px-6 py-4'>
