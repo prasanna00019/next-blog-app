9c63ea5ccabf1c03bc9d4320164ff88e9c944c02
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-07-15 13:58:15 +0530

    Update page.jsx

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 
-const page = () => {
+const Page = () => {
   const [emails,setemails]=useState([]);
   const fetchEmails=async()=>{
     const response=await axios.get('/api/email');
@@ -56,4 +56,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
